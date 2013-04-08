08391c513c708d35b207b0b56e1dc989039ddf09
Khaerul Adzany <khaerul.adzany@gmail.com>
2013-04-08 15:16:09 +0700

    Another afternoon commit

--- PrayTimeCs/PrayTimeCs.cs
+++ PrayTimeCs/PrayTimeCs.cs
@@ -5,53 +5,40 @@ using System.Text;
 
 namespace PrayTimeCs
 {
-    // PrayTimeCs class
+    public static class TimeNames
+    {
+        public static string Imsak = "Imsak";
+        public static string Fajr = "Fajr";
+        public static string Sunrise = "Sunrise";
+        public static string Dhuhr = "Dhuhr";
+        public static string Asr = "Asr";
+        public static string Sunset = "Sunset";
+        public static string Maghrib = "Maghrib";
+        public static string Isha = "Isha";
+        public static string Midnight = "Midnight";
+    }
+    public static class HighLatMethods
+    {
+        public static string NightMiddle = "NightMiddle";   // middle of night
+        public static string AngleBased = "AngleBased";     // angle/60th of night
+        public static string OneSeventh = "OneSeventh";     // 1/7th of night
+        public static string None = "None";                 // No adjustment
+    }
+    public class Param
+    {
+        public string ParamName { get; set; }
+        public double? DegValue { get; set; }
+        public double? MinuteValue { get; set; }
+        public string DescribedValue { get; set; }
+    }
+    public class Method
+    {
+        public string Name { get; set; }
+        public List<Param> Params { get; set; }
+    }
     public class PrayTimeCs
     {
-        #region Constants & Structures
-        public static class TimeNames
-        {
-            public static string Imsak = "Imsak";
-            public static string Fajr = "Fajr";
-            public static string Sunrise = "Sunrise";
-            public static string Dhuhr = "Dhuhr";
-            public static string Asr = "Asr";
-            public static string Sunset = "Sunset";
-            public static string Maghrib = "Maghrib";
-            public static string Isha = "Isha";
-            public static string Midnight = "Midnight";
-        }
-        public static class HighLatMethods
-        {
-            public static string NightMiddle = "NightMiddle";   // middle of night
-            public static string AngleBased = "AngleBased";     // angle/60th of night
-            public static string OneSeventh = "OneSeventh";     // 1/7th of night
-            public static string None = "None";                 // No adjustment
-        }
-        public class Param
-        {
-            public string ParamName { get; set; }
-            public float? DegValue { get; set; }
-            public float? MinuteValue { get; set; }
-            public string DescribedValue { get; set; }
-        }
-        public class Method
-        {
-            public string Name { get; set; }
-            public List<Param> Params { get; set; }
-        }
-        public class ParamsComparer : IEqualityComparer<Param>
-        {
-            public bool Equals(Param x, Param y)
-            {
-                return x.ParamName == y.ParamName;
-            }
-
-            public int GetHashCode(Param obj)
-            {
-                return obj.GetHashCode();
-            }
-        }
+        #region Constants        
         private Method _calcMethod;
         private Dictionary<String, Method> _methods;
         private List<Param> _defaultParams = new List<Param>
@@ -94,7 +81,7 @@ namespace PrayTimeCs
         private string[] _timeSuffixes = new string[] { "am", "pm" };
         private string _invalidTime = "-----";
         private int _numIterations = 1;
-        private Dictionary<String, float> _offset = new Dictionary<string, float>
+        private Dictionary<String, double> _offset = new Dictionary<string, double>
         {
             { TimeNames.Imsak, 0 },
             { TimeNames.Fajr, 0 },
@@ -106,8 +93,8 @@ namespace PrayTimeCs
             { TimeNames.Isha, 0 },
             { TimeNames.Midnight, 0 }
         }; 
-        private float _lat, _lng, _elv;
-        private float _timeZone, _jDate;
+        private double _lat, _lng, _elv;
+        private double _timeZone, _jDate;
         #endregion
         
         #region Initializers & Public Methods
@@ -202,12 +189,12 @@ namespace PrayTimeCs
                         new Param
                         {
                             ParamName = TimeNames.Fajr,
-                            DegValue = 19.5f
+                            DegValue = 19.5
                         },
                         new Param
                         {
                             ParamName = TimeNames.Isha,
-                            DegValue = 17.5f
+                            DegValue = 17.5
                         }
                     }
                 });
@@ -220,7 +207,7 @@ namespace PrayTimeCs
                         new Param
                         {
                             ParamName = TimeNames.Fajr,
-                            DegValue = 19.5f
+                            DegValue = 19.5
                         },
                         new Param
                         {
@@ -256,7 +243,7 @@ namespace PrayTimeCs
                             new Param
                             {
                                 ParamName = TimeNames.Fajr,
-                                DegValue = 17.7f
+                                DegValue = 17.7
                             },
                             new Param
                             {
@@ -266,7 +253,7 @@ namespace PrayTimeCs
                             new Param
                             {
                                 ParamName = TimeNames.Maghrib,
-                                DegValue = 4.5f
+                                DegValue = 4.5
                             },
                             new Param
                             {
@@ -304,46 +291,106 @@ namespace PrayTimeCs
                             }
                     });
         }
-
         public void SetMethod(Method method)
         {
-            
+            _calcMethod = method;
         }
-
         public void Adjust(List<Param> param)
         {
-
+            _settings = param;
         }
-
-        public void Tune(Dictionary<String, float> offset)
+        public void Tune(Dictionary<String, double> offset)
         {
-
+            _offset = offset;
         }
-
         public Method GetMethod()
         {
             return _calcMethod;
         }
-
         public List<Param> GetSetting()
         {
             return _settings;
         }
-
-        public Dictionary<String, float> GetOffset()
+        public Dictionary<String, double> GetOffset()
         {
             return _offset;
         }
-
         public Dictionary<String, Method> GetDefaults()
         {
             return _methods;
         }
-
-        public List<String> GetTimes()
+        public List<String> GetTimes(DateTime date, double lat, double lng, double timezone, int dst)
         {
             List<String> resultingTimes = new List<string>();
-            return resultingTimes;
+            _lat = lat;
+            _lng = lng;
+            //TODO: timezone functions (DST, GMTOffsett, etc is not yet implemented
+            _timeZone = 1 * timezone + ((1 * dst) > 0 ? 1 : 0);
+            _jDate = GetJulian(date.Year, date.Month, date.Day) - _lng / (15 * 24);
+            return ComputeTimes(resultingTimes);
+        }
+        private List<string> ComputeTimes(List<string> resultingTimes)
+        {
+            Dictionary<String, double> times = new Dictionary<string, double>
+            {
+                { TimeNames.Imsak, 5 },
+                { TimeNames.Fajr, 5 },
+                { TimeNames.Sunrise, 6 },
+                { TimeNames.Dhuhr, 12 },
+                { TimeNames.Asr, 13 },
+                { TimeNames.Sunset, 18 },
+                { TimeNames.Maghrib, 18 },
+                { TimeNames.Isha, 18 },
+                { TimeNames.Midnight, 0 }
+            }; 
+
+            // main iterations
+            for (var i = 1; i <= _numIterations; i++)
+                times = this.ComputePrayerTimes(times);
+
+            times = this.AdjustTimes(times);
+
+            // add midnight time
+            times[TimeNames.Midnight] = 
+                (_settings.Where( c => c.ParamName == TimeNames.Midnight).FirstOrDefault().DescribedValue == "Jafari") ?
+				times[TimeNames.Sunset] + TimeDiff(times[TimeNames.Sunset], times[TimeNames.Fajr]) / 2 :
+                times[TimeNames.Sunset] + TimeDiff(times[TimeNames.Sunset], times[TimeNames.Sunrise]) / 2;
+
+            times = this.TuneTimes(times);
+            return this.ModifyFormats(times);
+        }
+        private int TimeDiff(double starTime, double endTime)
+        {
+            throw new NotImplementedException();
+        }
+        private Dictionary<string,double> TuneTimes(Dictionary<string,double> times)
+        {
+            throw new NotImplementedException();
+        }
+        private List<string> ModifyFormats(Dictionary<string,double> times)
+        {
+ 	        throw new NotImplementedException();
+        }
+        private Dictionary<string,double> AdjustTimes(Dictionary<string,double> times)
+        {
+ 	        throw new NotImplementedException();
+        }
+        private Dictionary<string,double> ComputePrayerTimes(Dictionary<string,double> times)
+        {
+ 	        throw new NotImplementedException();
+        }
+        private double GetJulian(int year, int month, int day)
+        {
+            if (month <= 2)
+            {
+                year -= 1;
+                month += 12;
+            };
+            double A = Math.Floor((double)(year / 100));
+            double B = 2 - A + Math.Floor(A / 4);
+
+            double JD = (double)(Math.Floor((double)(365.25 * (year + 4716))) + Math.Floor((double)(30.6001 * (month + 1))) + day + B - (double)1524.5);
+            return JD;
         }
         #endregion
     }
