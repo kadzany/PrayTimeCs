4cda46698a0a1ef3542b47f95a098bfda62c3f6a
Khaerul Adzany <khaerul.adzany@gmail.com>
2013-04-12 16:49:37 +0700

    Added checking to Next Prayer Time

--- PrayTimeCsCmd/Program.cs
+++ PrayTimeCsCmd/Program.cs
@@ -9,8 +9,17 @@ namespace PrayTimeCsCmd
     {
         static void Main(string[] args)
         {
+            Dictionary<string, DateTime> result = new Dictionary<string, DateTime>();
             PrayTimeCs.PrayTimeCs prayTimeCs = new PrayTimeCs.PrayTimeCs("Egypt");
-            prayTimeCs.GetTimes(DateTime.Now, -6.910237756030072, 107.59923934936523, 7, 0);
+            result = prayTimeCs.GetTimes(DateTime.Now, -6.910237756030072, 107.59923934936523, 7, 0);
+            foreach (var r in result)
+            {
+                if (DateTime.Now < r.Value)
+                {
+                    Console.WriteLine("Next Prayer is: {0} ({1})", r.Key, r.Value.ToShortTimeString());
+                    break;
+                }
+            }
         }
     }
 }
